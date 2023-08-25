68ef0894b78ef3d498830dff4838dd0c5ee6639a
klandestino7 <klandestino6@hotmail.com>
2023-08-25 18:15:54 -0300

    Update video.section.tsx

--- src/views/home/video.section.tsx
+++ src/views/home/video.section.tsx
@@ -10,7 +10,7 @@ const Video = () => {
 
     return (
         <Flex
-            marginTop={{ base: "10vh", md : "5vh"}}
+            marginTop={{ base: "15vh", md : "10vh"}}
             marginBottom={{ base: "5vh", md : "0"}}
             padding={{base: "0 2vh", md: "2vh" }}
             h={{ base: "auto", md: "80vh"}}
