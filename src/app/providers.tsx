26f92b21461730aa5acca98b2b09f0cb430ba76a
klandestino7 <klandestino6@hotmail.com>
2023-08-25 21:55:49 -0300

    update tag

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -21,18 +21,18 @@ export function Providers({
         <QueryClientProvider client={queryClient}>
             <CacheProvider>
                 <ChakraProvider theme={theme}>
-                    {/* <Script async src="https://www.googletagmanager.com/gtag/js?id=G-SGFPSN2KPW" /> */}
+                    {/* <Script async src="https://www.googletagmanager.com/gtag/js?id=G-42TSM0LLHD" /> */}
                     <Script id="google-analytics">
                         {`
                         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                        })(window,document,'script','dataLayer','G-SGFPSN2KPW');
+                        })(window,document,'script','dataLayer','G-42TSM0LLHD');
                         `}
                     </Script>
 
-                    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=G-SGFPSN2KPW"
+                    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=G-42TSM0LLHD"
                         height="0" width="0" style={{
                             display: "none", visibility: "hidden"
                         }}></iframe>
